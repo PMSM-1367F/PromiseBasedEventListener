1e6fbe1ca6d334bf4cb3a6909e4937e4ea913786
KQ1367 <pmsm1372vvvf@gmail.com>
2025-07-12 15:56:36 +0900

    Update Signal-ES6.js

--- 26(Promise based event listener)/scripts/ES6/Signal-ES6.js
+++ 26(Promise based event listener)/scripts/ES6/Signal-ES6.js
@@ -143,26 +143,3 @@ class EventSignal extends Signal {
         }, options));
     }
 }
-
-const btn1 = document.querySelector('#btn1');
-const btn2 = document.querySelector('#btn2');
-const btn3 = document.querySelector('#btn3');
-
-EventSignal.addEventListenerTo(btn1, 'click')
-    .receive(data => {
-        console.log(data, 'first');
-        return EventSignal.addEventListenerTo(btn2, 'click');
-    })
-    .receive(data => {
-        console.log(data, 'second');
-        return EventSignal.addEventListenerTo(btn3, 'click');
-    })
-    .receive(data => console.log(data, 'third'));
-
-new Signal(raise => {
-    const ID = setInterval(() => raise(ID), 1000);
-})
-    .receive(data => {
-        console.log(data, 'setInterval');
-        setTimeout(() => clearInterval(data), 10000);
-    });
